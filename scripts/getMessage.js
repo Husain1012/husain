38bd89239f83c418077b7f00425d78523bff1796
Husain1012 <hussainmhod384@gmail.com>
2024-09-03 08:58:06 +0000

    feat: initiated the project

---
+++ scripts/getMessage.js
@@ -0,0 +1,7 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+
+const sendShieldedQuery = async (provider, destination, data) => {
+  const rpclink = hre.network.config.url;
+  const [encryptedData, usedEncryptedKey] = await encryptDataField(rpclink, data);
+  const response = await provider.call({
